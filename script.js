578f6329f130255683039afabd81e695238aaf4b
abirhasan01 <appleabirhasan@gmail.com>
2024-05-15 02:21:21 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// @ts-nocheck
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+ScrollReveal().reveal(".main-titel, .section-titel", {
+  delay: 500,
+  origin: "left",
+});
+ScrollReveal().reveal(".sec-1 .image, .info", {
+  delay: 600,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".text-box", {
+  delay: 700,
+  origin: "right",
+});
+ScrollReveal().reveal(".media-icons i", {
+  delay: 500,
+  origin: "bottom",
+  interval: 200,
+});
+ScrollReveal().reveal(".sec-2 .image, .sec-3 .image", {
+  delay: 500,
+  origin: "top",
+});
+ScrollReveal().reveal(".media-info li", {
+  delay: 500,
+  origin: "left",
+  interval: 200,
+});
